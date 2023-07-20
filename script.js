a60782790f078751ccfa63bea0da190a9b267f9d
sarperxx <43006015+sarperxx@users.noreply.github.com>
2023-07-20 20:17:08 +0300

    project finished

--- script.js
+++ script.js
@@ -6,27 +6,22 @@ function playRound(playerSelection, computerSelection) {
     document.getElementById("text").textContent =
       "You've won rock beats scissors";
     PlayerScore += 1;
-    alert(`You:${PlayerScore} computer:${ComputerScore}`);
   } else if (playerSelection == "scissors" && computerSelection == "paper") {
     document.getElementById("text").textContent =
       "You've won scissors beats paper";
     PlayerScore += 1;
-    alert(`You:${PlayerScore} computer:${ComputerScore}`);
   } else if (playerSelection == "paper" && computerSelection == "rock") {
     document.getElementById("text").textContent = "You've won paper beats rock";
     PlayerScore += 1;
-    alert(`You:${PlayerScore} computer:${ComputerScore}`);
   } else if (playerSelection == computerSelection) {
     document.getElementById("text").textContent = "tie";
-    alert(`You:${PlayerScore} computer:${ComputerScore}`);
   } else if (playerSelection == "") {
-    alert("invalid move");
+    return;
   } else {
     document.getElementById(
       "text"
     ).textContent = `You've lost ${computerSelection} beats ${playerSelection}`;
     ComputerScore += 1;
-    alert(`You:${PlayerScore} computer:${ComputerScore}`);
   }
 }
 
@@ -36,24 +31,25 @@ function getComputerChoice() {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 //decide who is winner
-function decideWinner() {
-  if (PlayerScore > ComputerScore) {
-    alert(`You:${PlayerScore} computer:${ComputerScore} You won.`);
-  } else if (PlayerScore == ComputerScore) {
-    alert(`You:${PlayerScore} computer:${ComputerScore} tie.`);
-  } else {
-    alert(`You:${PlayerScore} computer:${ComputerScore} You lose.`);
-  }
-}
+
 document.addEventListener("click", (img) => {
   let elementClass = img.target.className;
   let playerSelection = elementClass;
-  console.log(elementClass);
   const computerSelection = getComputerChoice();
-  console.log(computerSelection);
-  console.log(playerSelection);
   console.log(playRound(playerSelection, computerSelection));
+  document.querySelector(".plascore").textContent = PlayerScore;
+  document.querySelector(".compscore").textContent = ComputerScore;
+  if (PlayerScore == 5 || ComputerScore == 5) {
+    if (PlayerScore == 5) {
+      document.getElementById("text").textContent = "You've won";
+      PlayerScore = 0;
+      ComputerScore = 0;
+    } else if (ComputerScore == 5) {
+      document.getElementById("text").textContent = "Computer won";
+      PlayerScore = 0;
+      ComputerScore = 0;
+    }
+  }
 });
 
 // print out who is winner
-console.log(decideWinner());
